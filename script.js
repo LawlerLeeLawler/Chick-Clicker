438012d23370f0534eb785f3a3b7e5b1dd9c387a
BroskiRadical <elijahlgriffin@hotmail.com>
2026-07-11 17:41:35 -0500

    Added starter files for website
    Added index.html, script.js, style.css, and screenshots.

---
+++ script.js
@@ -0,0 +1,15 @@
+const button =
+    document.getElementById("wishlistButton");
+
+const downloadButton =
+    document.getElementById("downloadButton");
+
+button.addEventListener("click", function()
+{
+    alert("Steam page coming soon!");
+});
+
+downloadButton.addEventListener("click", function()
+{
+    alert("Download page coming soon!");
+});
